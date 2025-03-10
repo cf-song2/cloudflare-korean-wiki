211bb07b430cc80a06877e4193a0332c2e7240e3
cf-song2 <cfsong296@gmail.com>
2025-03-10 22:13:37 +0900

    modified: UI

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -1,21 +1,23 @@
 const md = window.markdownit();
 
-function toggleMenu(id) {
-    let menu = document.getElementById(id);
-    menu.style.display = (menu.style.display === "block") ? "none" : "block";
-}
-
 function toggleSubmenu(id) {
     let submenu = document.getElementById(id);
-    submenu.style.display = (submenu.style.display === "block") ? "none" : "block";
+    let parentItem = submenu.previousElementSibling;
+
+    if (submenu.style.display === "block") {
+        submenu.style.display = "none";
+        parentItem.classList.remove("open");
+    } else {
+        submenu.style.display = "block";
+        parentItem.classList.add("open");
+    }
 }
 
 function loadMarkdown(file) {
     fetch(file)
         .then(response => response.text())
         .then(markdown => {
-            const htmlContent = md.render(markdown);
-            document.getElementById("content").innerHTML = htmlContent;
+            document.getElementById("content").innerHTML = md.render(markdown);
         })
         .catch(error => {
             document.getElementById("content").innerHTML = "<p>페이지를 불러오는 데 실패했습니다.</p>";
